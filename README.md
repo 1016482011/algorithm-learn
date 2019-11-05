964e02649388e960490e50ecb84bb4d2a907c3d5
clong <2956957982@qq.com>
2019-11-05 11:27:07 +0800

    添加单链

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # algorithm-learn
+
 算法学习
 
-根据[课程中的案例](https://time.geekbang.org/column/intro/126)的思路，自己实现一遍。
+根据[课程中的案例](https://time.geekbang.org/column/intro/126)的思路，自己用 js 实现一遍。
+
+## 案例
+
+- [单链]()
---
+++ linkedList/singlyLinkedList.js
@@ -0,0 +1,91 @@
+// js实现单链
+class Node {
+  constructor(el) {
+    this.el = el
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = new Node('head')
+  }
+  // 根据值查找节点
+  findByValue(val) {
+    let currentNode = this.head.next
+    while (currentNode !== null && currentNode.el !== val) {
+      currentNode = currentNode.next
+    }
+    console.log(currentNode)
+    return currentNode === null ? -1 : currentNode
+  }
+  // 根据索引查找节点
+  findByIndex(index) {
+    let pos = 0
+    let currentNode = this.head.next
+    while (currentNode !== null && pos !== index) {
+      currentNode = currentNode.next
+      pos++
+    }
+    console.log(currentNode)
+    return currentNode === null ? -1 : currentNode
+  }
+  // 查找前一个值
+  findPrev(val) {
+    let currentNode = this.head
+    while (currentNode.next !== null && currentNode.next.el !== val) {
+      currentNode = currentNode.next
+    }
+    console.log(currentNode)
+    if (currentNode.next === null) {
+      return -1
+    }
+
+    return currentNode
+  }
+  // 链尾添加节点
+  append(newEl) {
+    const newNode = new Node(newEl)
+    let currentNode = this.head
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next
+    }
+    currentNode.next = newNode
+  }
+  // 插入节点
+  insert(newEl, el) {
+    const newNode = new Node(newEl)
+    const targetNode = this.findByValue(el)
+    if (targetNode === -1) {
+      console.log('对应节点不存在')
+      return
+    }
+    newNode.next = targetNode.next
+    targetNode.next = newNode
+  }
+  // 根据值删除节点
+  remove(val) {
+    const preNode = this.findPrev(val)
+    if (preNode === -1) {
+      console.log('节点不存在')
+      return
+    }
+    preNode.next = preNode.next.next
+  }
+
+  // 显示链表数据
+  display() {
+    let currentNode = this.head.next
+    while (currentNode !== null) {
+      console.log(currentNode.el)
+      currentNode = currentNode.next
+    }
+  }
+}
+
+const LList = new LinkedList()
+LList.append('f')
+LList.append('head')
+LList.append('t')
+LList.insert('sdsd', 'head')
+LList.display()
