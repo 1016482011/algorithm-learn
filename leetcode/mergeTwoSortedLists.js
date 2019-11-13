f5a81754410a6116634aa96502ef1125eec08d75
clong <2956957982@qq.com>
2019-11-13 19:09:32 +0800

    添加链表合并解法二

--- leetcode/mergeTwoSortedLists.js
+++ leetcode/mergeTwoSortedLists.js
@@ -32,3 +32,30 @@ var mergeTwoLists = function(l1, l2) {
   if (currentNode2 !== null) node.next = currentNode2;
   return headNode.next;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists1 = function(l1, l2) {
+  function merge(list1, list2) {
+    if (list1 === null) return list2;
+    if (list2 === null) return list1;
+    if (list1.val < list2.val) {
+      list1.next = merge(list1.next, list2);
+      return list1;
+    } else {
+      list2.next = merge(list1, list2.next);
+      return list2;
+    }
+  }
+  return merge(l1, l2);
+};
