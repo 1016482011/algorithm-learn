2c703f57538ce61ed2cd616ada7f61a4c88dbd9e
clong <2956957982@qq.com>
2019-11-26 16:52:11 +0800

    求平方根

--- binary/squareRoot.js
+++ binary/squareRoot.js
@@ -1,27 +1,29 @@
 // 求一个数的平方根，精确到小数点后 6 位
 function squareRoot(n) {
-  if (n <= 0) return
+  if (n <= 0) return;
   let low = 0,
     high = n,
-    mid
+    mid;
   for (let i = 0; i <= 6; i++) {
-    let pow = Math.pow(10, -i)
-    mid = low + (high - low) / 2
-    midSquare = mid * mid
+    let pow = Math.pow(10, -i);
+    mid = high;
+    midSquare = mid * mid;
     while (!(n - midSquare > 0 && n - midSquare < pow)) {
-      if (midSquare === n) return mid.toFixed(6)
+      if (midSquare === n) return mid.toFixed(6);
       if (midSquare > n) {
-        high = mid
+        high = mid;
       } else {
-        low = mid
+        low = mid;
       }
-      mid = low + (high - low) / 2
-      midSquare = mid * mid
+      mid = low + (high - low) / 2;
+      midSquare = mid * mid;
     }
   }
-  return mid.toFixed(6)
+  return mid.toFixed(6);
 }
 
-console.log(squareRoot(4))
-console.log(squareRoot(9))
-console.log(squareRoot(10))
+console.log(squareRoot(4));
+console.log(squareRoot(9));
+console.log(squareRoot(8));
+console.log(squareRoot(10));
+console.log(squareRoot(2147483647));
---
+++ leetcode/sqrtx.js
@@ -0,0 +1,53 @@
+/**
+ * https://leetcode-cn.com/problems/sqrtx/
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(n) {
+  if (n === 0) return 0;
+  let low = 0,
+    high = n,
+    mid;
+  for (let i = 0; i <= 6; i++) {
+    let pow = Math.pow(10, -i);
+    mid = n;
+    midSquare = mid * mid;
+    while (!(n - midSquare > 0 && n - midSquare < pow)) {
+      if (midSquare === n) return parseInt(mid.toFixed(6));
+      if (midSquare > n) {
+        high = mid;
+      } else {
+        low = mid;
+      }
+      mid = low + (high - low) / 2;
+      midSquare = mid * mid;
+    }
+  }
+  return parseInt(mid.toFixed(6));
+};
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+  if (x == 1 || x == 0) {
+    return x;
+  }
+  let start = 1;
+  let end = x / 2 + 1;
+  let mid = 0;
+  while (start <= end) {
+    mid = parseInt(start + (end - start) / 2);
+    //防止越界
+    if (mid <= x / mid && mid + 1 > x / (mid + 1)) {
+      return mid;
+    }
+    if (mid > x / mid) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return mid;
+};
